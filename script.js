fb3a8c1c0a31489bd4d2375e37c7561ff293d253
safluke <saf_2356@outlook.com>
2022-09-04 12:07:23 +0100

    project section design and nav bar top

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// JavaScript source code
+const sections = document.querySelectorAll('section')
+const navli = document.querySelectorAll("nav .container ul li")
+
+window.addEventListener('scroll', () => {
+    let current = ""
+    console.log(pageYOffset)
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop //calculate distance from page top to section top
+        const sectionHeight = section.clientHeight
+        if (pageYOffset >= sectionTop - 300) {
+            current = section.getAttribute('id')
+        }
+    })
+    navli.forEach(li => {
+        li.classList.remove("active")
+        if (li.classList.contains(current)) {
+            li.classList.add("active")
+        }
+    })
+})
